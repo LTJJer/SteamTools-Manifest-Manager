05105a5e7b3a43ca8cd83f194bb94ec6f60df6b9
LT_JJ <lt_jjer@163.com>
2026-05-12 14:03:13 +0800

    feat(export-dialog): 添加保存导出设置功能中的读取设置

--- exportdialog.cpp
+++ exportdialog.cpp
@@ -1,6 +1,8 @@
 #include "exportdialog.hpp"
 #include "ui_exportdialog.h"
 
+#include "constant.hpp"
+
 
 
 ExportDialog::ExportDialog(const QString &sourceLuaPath, QWidget *parent)
@@ -9,6 +11,41 @@ ExportDialog::ExportDialog(const QString &sourceLuaPath, QWidget *parent)
     , mv_sourceLuaPath(sourceLuaPath)
 {
     ui->setupUi(this);
+
+
+
+    ui->btng_FileNameMode->setId(ui->rdo_Normal_ChangeFileName_Appid, FileNameChangeMode::Appid);
+    ui->btng_FileNameMode->setId(ui->rdo_Normal_ChangeFileName_Name, FileNameChangeMode::Name);
+    ui->btng_ContentProcessMode->setId(ui->rdo_Normal_ContentProcess_Format, ContentProcessMode::Format);
+    ui->btng_ContentProcessMode->setId(ui->rdo_Normal_ContentProcess_FormatNoInfo, ContentProcessMode::FormatNoInfo);
+    ui->btng_ContentProcessMode->setId(ui->rdo_Normal_ContentProcess_OnlyInfo, ContentProcessMode::OnlyInfo);
+
+
+
+    // 用户配置
+    {
+        ui->tab_ExportMode->setCurrentIndex(Constant::getSettings().value("Export/ExportMode", 0).toInt());
+
+        // 普通
+        {
+            ui->gbx_Normal_ChangeFileName->setChecked(Constant::getSettings().value("Export/Normal/isChangeFileName", true).toBool());
+            ui->btng_FileNameMode->button(Constant::getSettings().value("Export/Normal/FileNameMode", FileNameChangeMode::Name).value<FileNameChangeMode>())->setChecked(true);
+
+            ui->gbx_Normal_ContentProcess->setChecked(Constant::getSettings().value("Export/Normal/isContentProcess", false).toBool());
+            ui->btng_ContentProcessMode->button(Constant::getSettings().value("Export/Normal/ContentProcessMode", ContentProcessMode::Format).value<ContentProcessMode>())->setChecked(true);
+
+            ui->le_Normal_Dir->setText(Constant::getSettings().value("Export/Normal/Dir", "").toString());
+        }
+
+        // 合并
+        {
+            ui->gbx_Merge_WriteInfo->setChecked(Constant::getSettings().value("Export/Merge/isWriteInfo", false).toBool());
+
+            ui->le_Merge_Path->setText(Constant::getSettings().value("Export/Merge/Path", "").toString());
+        }
+
+        ui->chk_OpenFolder->setChecked(Constant::getSettings().value("Export/OpenFolder", true).toBool());
+    }
 }
 
 ExportDialog::~ExportDialog()
--- exportdialog.hpp
+++ exportdialog.hpp
@@ -12,6 +12,19 @@ class ExportDialog : public QDialog
 {
     Q_OBJECT
 
+private:
+    enum FileNameChangeMode
+    {
+        Appid,
+        Name,
+    };
+    enum ContentProcessMode
+    {
+        Format,
+        FormatNoInfo,
+        OnlyInfo,
+    };
+
 public:
     explicit ExportDialog(const QString &sourceLuaPath, QWidget *parent = nullptr);
     ~ExportDialog();
